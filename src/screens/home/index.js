e2778eaff51e14b94df6b16dc0f8d8fe81bdb24e
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-05 12:59:27 +0800

    :construction: Use temporary TopBar

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -5,15 +5,7 @@ import Container from "../../components/container";
 export default function HomeScreen({ navigation }) {
     return (
         <Container>
-            <TopBar
-                type={2}
-                title="Book Appointment"
-                subtitle="Petsville Animal Clinic, Cyberjaya"
-                leftIcon="chevron-left"
-                leftIconProps={{ onPress: () => navigation.goBack() }}
-                rightIcon="bell"
-                rightIconProps={{ onPress: () => alert("Damn!") }}
-            />
+            <TopBar type={2} rightIcon="bell" rightIconProps={{ onPress: () => alert("Opening notifications..") }} />
         </Container>
     );
 }
