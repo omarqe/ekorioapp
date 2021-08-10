53e633dad706a79f329675bb5da5bf5675d95739
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-10 19:35:26 +0800

    :recycle: Allow badge in heading text

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,12 +1,13 @@
 import React from "react";
 import CT from "../const.js";
+import Badge from "./badge";
 import PropTypes from "prop-types";
 import { View, Text, StyleSheet } from "react-native";
 
 import _omit from "lodash/omit";
 
 const Heading = (props) => {
-    const { size = 0, text, subtitle } = props;
+    const { size = 0, text, badge = null, subtitle } = props;
     const appendedProps = _omit(props, ["size", "text", "subtitle"]);
     const titleSizes = [20, 22, 24, 28, 32];
     const subtitleSizes = [14, 16, 18, 20, 24];
@@ -14,9 +15,16 @@ const Heading = (props) => {
     let textStyle = { ...ss.text, fontSize: titleSizes[size] };
     let subtitleStyle = { ...ss.subtitle, fontSize: subtitleSizes[size] };
 
+    if (badge !== null) {
+        badge.style = { ...badge?.style, marginLeft: 5 };
+    }
+
     return (
         <View {...appendedProps}>
-            <Text style={textStyle}>{text}</Text>
+            <View style={ss.title}>
+                <Text style={textStyle}>{text}</Text>
+                {badge && <Badge sm {...badge} />}
+            </View>
             {subtitle && <Text style={subtitleStyle}>{subtitle}</Text>}
         </View>
     );
@@ -25,10 +33,16 @@ const Heading = (props) => {
 Heading.propTypes = {
     size: PropTypes.oneOf([0, 1, 2, 3, 4]),
     text: PropTypes.string,
+    badge: PropTypes.object,
     subtitle: PropTypes.string,
 };
 
 const ss = StyleSheet.create({
+    title: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+    },
     text: {
         color: CT.BG_GRAY_900,
         fontWeight: "700",
