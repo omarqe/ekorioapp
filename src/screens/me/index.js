7b150a49b6aa852b5d89f533ac2fa8e31f7e5946
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-12 01:41:43 +0800

    :construction: Working on me screen

--- src/screens/me/index.js
+++ src/screens/me/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 import CT from "../../const";
-import TopBar from "../../components/topbar";
+
+import Body from "../../components/layout/body";
+import Layout from "../../components/layout";
 import Header from "../../components/layout/header";
+
+import TopBar from "../../components/topbar";
 import Heading from "../../components/heading";
 import Container from "../../components/container";
+
 import { StyleSheet } from "react-native";
 
 const MeScreen = () => {
     return (
         <Container>
-            <TopBar type={2} title="Me" />
-            <Header>
-                <Heading
-                    text="Eve Harrison"
-                    subtitle="Joined 2 weeks ago"
-                    textStyle={ss.headerText}
-                    subtitleStyle={ss.headerSub}
-                />
-            </Header>
+            <TopBar type={2} />
+            <Layout gray withHeader>
+                <Header>
+                    <Heading
+                        text="Eve Harrison"
+                        subtitle="Joined 2 weeks ago"
+                        textStyle={ss.headerText}
+                        subtitleStyle={ss.headerSub}
+                    />
+                </Header>
+                <Body gray flex></Body>
+            </Layout>
         </Container>
     );
 };
