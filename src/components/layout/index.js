1ea1c42a79011bd4347e61ee89a1adcf6e12770d
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-12 15:01:00 +0800

    :recycle: Update component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -6,19 +6,21 @@ import { View, ScrollView, StyleSheet } from "react-native";
 import _omit from "lodash/omit";
 
 const Layout = (props) => {
-    let { base = "white", gray = false, withHeader = false, children } = props;
+    let { base = "white", gray = false, withHeader = false, scrollProps = {}, children } = props;
     const appendedProps = _omit(props, ["base", "children", "withHeader"]);
 
     if (gray) base = "gray";
     const baseColor = { white: CT.BG_WHITE, gray: CT.BG_GRAY_50, purple: CT.BG_PURPLE_900 };
-    const topStyle = { ...styles.mirage, top: 0, backgroundColor: withHeader ? baseColor.purple : baseColor[base] };
-    const bottomStyle = { ...styles.mirage, bottom: 0, backgroundColor: baseColor[base] };
+    const topStyle = { ...styles.backdrop, top: 0, backgroundColor: withHeader ? baseColor.purple : baseColor[base] };
+    const bottomStyle = { ...styles.backdrop, bottom: 0, backgroundColor: baseColor[base] };
 
     return (
         <View style={styles.base} {...appendedProps}>
             <View style={topStyle} />
             <View style={bottomStyle} />
-            <ScrollView contentContainerStyle={styles.bodyContentContainer}>{children}</ScrollView>
+            <ScrollView contentContainerStyle={styles.bodyContentContainer} {...scrollProps}>
+                {children}
+            </ScrollView>
         </View>
     );
 };
@@ -27,6 +29,8 @@ Layout.propTypes = {
     base: PropTypes.oneOf(["gray", "white", "purple"]),
     gray: PropTypes.bool,
     withHeader: PropTypes.bool,
+    scrollProps: PropTypes.object,
+    contentProps: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
@@ -34,7 +38,7 @@ const styles = StyleSheet.create({
         flex: 1,
         position: "relative",
     },
-    mirage: {
+    backdrop: {
         width: "100%",
         height: "50%",
         position: "absolute",
