0e263d21b619c23da0d6fad648729f079a49bcda
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-13 16:41:26 +0800

    :construction: Working on screen

--- src/screens/appointment/index.js
+++ src/screens/appointment/index.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Body from "../../components/layout/body";
 import Layout from "../../components/layout";
-// import Header from "../../components/layout/header";
+import Header from "../../components/layout/header";
 
+import Tabs from "../../components/tabs";
 import TopBar from "../../components/topbar";
 import Container from "../../components/container";
 
 import { StyleSheet } from "react-native";
 
 const AppointmentScreen = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const tabs = [{ label: "General" }, { label: "Boarding" }, { label: "Surgery" }, { label: "Others" }];
+
     return (
         <Container>
             <TopBar type={1} title="Appointments" rightIcon="plus" rightIconProps={{ glow: true }} />
             <Layout gray withHeader>
-                {/* <Header></Header> */}
+                <Header style={styles.header}>
+                    <Tabs tabs={tabs} active={activeIndex} onPress={setActiveIndex} alwaysBounceHorizontal={false} />
+                </Header>
                 <Body gray flex></Body>
             </Layout>
         </Container>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    header: {
+        paddingBottom: 0,
+    },
+});
 
 export default AppointmentScreen;
