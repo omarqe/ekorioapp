a4d9375a8eb173fe1444676d7f5100cc16545a7a
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-15 00:44:44 +0800

    :bug: Fix unwanted alert

--- src/screens/pet/index.js
+++ src/screens/pet/index.js
@@ -81,7 +81,7 @@ const _PetScreen = ({ navigation }) => {
         const cancelButtonIndex = 2;
 
         showActionSheetWithOptions({ options, cancelButtonIndex }, (buttonIndex) => {
-            const cmd = [alert.bind(null, "Reevaluating health"), navigation.navigate("pet__health-records")];
+            const cmd = [alert.bind(null, "Reevaluating health"), navigation.navigate.bind(null, "pet__health-records")];
             if (typeof cmd[buttonIndex] === "function") {
                 cmd[buttonIndex]();
             }
