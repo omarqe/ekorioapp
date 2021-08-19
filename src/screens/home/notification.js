91d40ebc2ae9ca353f230a2fd2608d94517a840c
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-20 01:11:26 +0800

    :sparkles: Add notification screen

--- src/screens/home/notification.js
+++ src/screens/home/notification.js
@@ -2,20 +2,39 @@ import React from "react";
 
 import Body from "../../components/layout/body";
 import Layout from "../../components/layout";
-// import Header from "../../components/layout/header";
 
+import List from "../../components/list";
 import TopBar from "../../components/topbar";
 import Container from "../../components/container";
 
 import { StyleSheet } from "react-native";
 
 const NotificationScreen = () => {
+    const data = [
+        {
+            text: "Nam laoreet lectus non sapien!",
+            icon: "cat",
+            subtitle: "Vestibulum tincidunt ligula lorem, at ullamcorper metus.",
+        },
+        {
+            text: "Suspendisse pellentesque porta!",
+            icon: "cat",
+            subtitle: "Cras ornare sit amet leo a sagittis nullam convallis.",
+        },
+        {
+            text: "Quisque posuere odio a magna",
+            icon: "cat",
+            subtitle: "Sed porttitor metus non molestie tristique vestibulum.",
+        },
+    ];
+
     return (
         <Container>
             <TopBar type={1} title="Notifications" />
             <Layout gray withHeader>
-                {/* <Header></Header> */}
-                <Body gray flex></Body>
+                <Body gray flex expanded>
+                    <List list={data} padded />
+                </Body>
             </Layout>
         </Container>
     );
