ca4176b58a9307998808e378b8ee3a87cc26a317
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-20 03:37:00 +0800

    :lipstick: Add variants

--- src/components/badge.js
+++ src/components/badge.js
@@ -6,33 +6,45 @@ import { View, Text, StyleSheet } from "react-native";
 import _omit from "lodash/omit";
 
 const Badge = (props) => {
-    let { style = {}, textStyle: labelStyle = {}, text, size = 1, xs = false, lg = false } = props;
-    let baseStyle = styles.base;
-    let textStyle = styles.text;
+    let { style = {}, textStyle: _textStyle = {} } = props;
+    let { text, size = 1, xs = false, lg = false, color = "default" } = props;
 
     // Handle size shortcuts
     if (xs) size = 0;
     else if (lg) size = 2;
 
-    // Control badge appearance based on size
-    switch (size) {
-        case 0:
-            baseStyle = { ...baseStyle, padding: 4, paddingLeft: 5, paddingRight: 6, borderRadius: 6 };
-            textStyle = { ...textStyle, fontSize: 12 };
-            break;
-        case 1:
-            baseStyle = { ...baseStyle, padding: 5, paddingLeft: 6, paddingRight: 6, borderRadius: 7 };
-            textStyle = { ...textStyle, fontSize: 14 };
-            break;
-        case 2:
-            baseStyle = { ...baseStyle, padding: 6, paddingLeft: 7, paddingRight: 7, borderRadius: 8 };
-            textStyle = { ...textStyle, fontSize: 16 };
-            break;
-    }
+    const sizes = [
+        {
+            baseStyle: { padding: 4, paddingLeft: 5, paddingRight: 6, borderRadius: 6 },
+            textStyle: { fontSize: 12 },
+        },
+        {
+            baseStyle: { padding: 5, paddingLeft: 6, paddingRight: 6, borderRadius: 7 },
+            textStyle: { fontSize: 14 },
+        },
+        {
+            baseStyle: { padding: 6, paddingLeft: 7, paddingRight: 7, borderRadius: 8 },
+            textStyle: { fontSize: 16 },
+        },
+    ];
+    const colors = {
+        default: { baseStyle: {}, textStyle: {} },
+        purple: {
+            baseStyle: { backgroundColor: CT.BG_PURPLE_500 },
+            textStyle: { color: CT.BG_PURPLE_100 },
+        },
+        yellow: {
+            baseStyle: { backgroundColor: CT.BG_YELLOW_500 },
+            textStyle: { color: CT.BG_YELLOW_900 },
+        },
+    };
+
+    const baseStyle = { ...styles.base, ...sizes[size]?.baseStyle, ...colors[color]?.baseStyle };
+    const textStyle = { ...styles.text, ...sizes[size]?.textStyle, ...colors[color]?.textStyle };
 
     return (
         <View style={[baseStyle, style]}>
-            <Text style={[textStyle, labelStyle]}>{text}</Text>
+            <Text style={[textStyle, _textStyle]}>{text}</Text>
         </View>
     );
 };
@@ -53,6 +65,7 @@ Badge.propTypes = {
     size: PropTypes.number,
     text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     style: PropTypes.object,
+    color: PropTypes.oneOf(["default", "purple", "yellow"]),
     textStyle: PropTypes.object,
 };
 
