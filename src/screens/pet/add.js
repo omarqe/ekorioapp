3dbf49d59f16dda8e815c24e3740d8d139173d88
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-20 18:45:07 +0800

    :sparkles: Complete pet form

--- src/screens/pet/add.js
+++ src/screens/pet/add.js
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import CT from "../../const";
 
+import PetTypes from "../../components/pet/pet-types";
 import Pet from "../../components/pet";
 import Badge from "../../components/badge";
+import Button from "../../components/button";
 import TopBar from "../../components/topbar";
+import Heading from "../../components/heading";
 import Container from "../../components/container";
+import FloatingFields from "../../components/floating-fields";
 
 import Body from "../../components/layout/body";
 import Layout from "../../components/layout";
 import Header from "../../components/layout/header";
 
-import { Text, View, StyleSheet } from "react-native";
+import _capitalize from "lodash/capitalize";
 
-export default function Template({ navigation }) {
+import { View, StyleSheet } from "react-native";
+
+export default function PetAddScreen({ navigation }) {
+    const [petType, setPetType] = useState("cat");
+    const fields = [
+        { label: "Name", value: "Cheshire" },
+        { label: "Microchip ID", placeholder: "" },
+        [
+            {
+                type: "select",
+                label: "Gender",
+                options: [
+                    { label: "Male", value: "male" },
+                    { label: "Female", value: "female" },
+                ],
+            },
+            {
+                type: "select",
+                label: "Breed",
+                options: [
+                    { label: "British Shorthair", value: "00001" },
+                    { label: "Maine Coon", value: "00002" },
+                ],
+            },
+        ],
+        [
+            { label: "Birthday", placeholder: "01/01/2021" },
+            { label: "Weight (kg)", type: "number", value: "2.50" },
+        ],
+        {
+            type: "textarea",
+            label: "Biography",
+        },
+    ];
+
     return (
         <Container>
-            <TopBar title="Add a Pet" leftIcon="arrow-left" leftIconProps={{ onPress: navigation.goBack }} />
+            <TopBar
+                title="Add a Pet"
+                leftIcon="arrow-left"
+                leftIconProps={{ onPress: navigation.goBack }}
+                rightIcon="ellipsis-h"
+            />
             <Layout gray withHeader>
                 <Header contentStyle={styles.headerContent} overlap>
                     <Pet
@@ -23,15 +66,24 @@ export default function Template({ navigation }) {
                         borderRadius={35}
                         padding={5}
                         size={130}
+                        image={require("../../../assets/pets/cat-04.png")}
                         nameStyle={styles.petName}
                         baseStyle={styles.petBase}
                         imageBaseStyle={styles.petImageBase}
                         phIconProps={{ color: CT.BG_PURPLE_200 }}
                     />
-                    <Badge xs text="Breed: Maine Coon" style={styles.petBadge} />
+                    <Badge text="Breed: Maine Coon" style={styles.petBadge} color="purple" />
                 </Header>
                 <Body gray flex overlap topRounded>
-                    <Text>asdadasdads</Text>
+                    <View style={styles.section}>
+                        <Heading text="Pet Type" />
+                        <PetTypes types={["cat", "dog", "rabbit", "bird"]} active={petType} onPress={setPetType} />
+                    </View>
+                    <View style={styles.section}>
+                        <Heading text="Pet Details" />
+                        <FloatingFields fields={fields} />
+                    </View>
+                    <Button label="Add Pet" />
                 </Body>
             </Layout>
         </Container>
@@ -39,13 +91,16 @@ export default function Template({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+    section: {
+        marginBottom: 20,
+    },
     headerContent: {
         alignItems: "center",
         flexDirection: "column",
     },
     petName: {
         color: CT.BG_PURPLE_100,
-        fontSize: 20,
+        fontSize: 22,
         fontWeight: "700",
         marginTop: 15,
     },
