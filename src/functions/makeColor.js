c79d78fe22df208c6340804a3ff8d6412fb4efb6
Omar Mokhtar <omar.veyron@gmail.com>
2021-08-25 07:00:45 +0800

    :lipstick: Update spectrum colors

--- src/functions/makeColor.js
+++ src/functions/makeColor.js
@@ -1,7 +1,7 @@
 export default function makeColor(value, over) {
     const x = 7 - Math.floor((value / over) * 7);
-    const text = ["#fff", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff"];
-    const background = ["#388E3C", "#689F38", "#AFB42B", "#FBC02D", "#FFA000", "#F57C00", "#E64A19", "#D32F2F"];
+    const text = ["#fff", "#fff", "#687300", "#948500", "#7A5C00", "#FFF6D9", "#fff", "#fff"];
+    const background = ["#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#F44336"];
 
     return { text: text[x], background: background[x] };
 }
