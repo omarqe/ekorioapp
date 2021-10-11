795c479c896141ac51faf10c4aa7344e52a34b7a
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-11 09:47:38 +0800

    :lipstick: Update component

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -6,8 +6,9 @@ import { LinearGradient } from "expo-linear-gradient";
 import { createShimmerPlaceholder } from "react-native-shimmer-placeholder";
 
 const ShimmerPlaceholder = createShimmerPlaceholder(LinearGradient);
-export default function Shimmer({ style, children, loading = true, shimmerStyle, ...restProps }) {
-    const colors = [CT.BG_GRAY_100, CT.BG_GRAY_100, CT.BG_GRAY_50];
+export default function Shimmer({ style, colors: c = null, children, loading = true, shimmerStyle, ...restProps }) {
+    const colors = c !== null ? c : [CT.BG_GRAY_100, CT.BG_GRAY_100, CT.BG_GRAY_50];
+
     return (
         <ShimmerPlaceholder visible={!loading} style={shimmerStyle} shimmerColors={colors} {...restProps}>
             <View style={style}>{children}</View>
@@ -17,5 +18,7 @@ export default function Shimmer({ style, children, loading = true, shimmerStyle,
 
 Shimmer.propTypes = {
     style: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+    colors: PropTypes.array,
     loading: PropTypes.bool,
+    shimmerStyle: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
 };
