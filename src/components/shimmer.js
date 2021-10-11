bd1287094ee1ae5988107f58c3cdce1543fa9262
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-11 09:08:21 +0800

    :lipstick: Remove opacity style

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,15 +1,15 @@
 import CT from "../const";
 import React from "react";
 import PropTypes from "prop-types";
+import { View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { View, StyleSheet } from "react-native";
 import { createShimmerPlaceholder } from "react-native-shimmer-placeholder";
 
 const ShimmerPlaceholder = createShimmerPlaceholder(LinearGradient);
 export default function Shimmer({ style, children, loading = true, shimmerStyle, ...restProps }) {
     const colors = [CT.BG_GRAY_100, CT.BG_GRAY_100, CT.BG_GRAY_50];
     return (
-        <ShimmerPlaceholder visible={!loading} style={[styles.base, shimmerStyle]} shimmerColors={colors} {...restProps}>
+        <ShimmerPlaceholder visible={!loading} style={shimmerStyle} shimmerColors={colors} {...restProps}>
             <View style={style}>{children}</View>
         </ShimmerPlaceholder>
     );
@@ -19,9 +19,3 @@ Shimmer.propTypes = {
     style: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
     loading: PropTypes.bool,
 };
-
-const styles = StyleSheet.create({
-    base: {
-        opacity: 0.7,
-    },
-});
