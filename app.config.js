4a87efdf41f7a907a238b808ec6975a57d1523cb
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-15 19:50:41 +0800

    :recycle: Update config

--- app.config.js
+++ app.config.js
@@ -3,7 +3,9 @@ export default {
     name: "Ekorio",
     version: "1.0.0",
     extra: {
+        DEVENV: process.env.DEVENV,
         API_HOST: process.env.API_HOST,
         API_SECRET: process.env.API_SECRET,
+        API_HOST_PORTABLE: process.env.API_HOST_PORTABLE,
     },
 };
