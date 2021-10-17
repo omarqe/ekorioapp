97ff40ab64d6134a7a91bdb97f05ff4cb6c1fbd3
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-17 16:01:40 +0800

    :recycle: Make toast appears longer

--- src/functions/toast.js
+++ src/functions/toast.js
@@ -1,7 +1,7 @@
 import Toast from "react-native-root-toast";
 import _get from "lodash/get";
 
-const defaultOptions = { duration: Toast.durations.SHORT, position: Toast.positions.BOTTOM - 20 };
+const defaultOptions = { duration: Toast.durations.LONG, position: Toast.positions.BOTTOM - 20 };
 export default {
     show: (message, options = {}) => Toast.show(message, { ...defaultOptions, ...options }),
     fromData: (data, path, options = {}) => Toast.show(_get(data, path), { ...defaultOptions, ...options }),
