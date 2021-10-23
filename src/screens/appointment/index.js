95f707790b3beccb554530adec6c38771312f12d
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-23 15:56:50 +0800

    :recycle: Fetch service types and appointment data

--- src/screens/appointment/index.js
+++ src/screens/appointment/index.js
@@ -15,10 +15,22 @@ import Container from "../../components/container";
 import { StyleSheet } from "react-native";
 import { TabView, SceneMap } from "react-native-tab-view";
 
+import net from "../../functions/net";
+import http from "../../functions/http";
+import moment from "moment";
+import _get from "lodash/get";
+import _clone from "lodash/clone";
 import _renderIf from "../../functions/renderIf";
 import _createSceneMap from "../../functions/createSceneMap";
 
-const Scene = ({ data, loading, onPress }) => {
+const Scene = ({ data = [], loading = false, onPress }) => {
+    if (loading) {
+        data = [];
+        for (let i = 1; i < 3; i++) {
+            data = [...data, { text: i.toString(), subtitle: null }];
+        }
+    }
+
     return (
         <Layout scrollEnabled={false} gray>
             <Body gray flex expanded>
@@ -33,101 +45,64 @@ const Scene = ({ data, loading, onPress }) => {
 };
 
 const AppointmentScreen = ({ navigation }) => {
-    const Time = () => (
-        <React.Fragment>
-            <Text style={styles.time}>
-                Fri, 13 Aug 2021 <Text style={{ color: CT.BG_GRAY_200 }}>@</Text> 3:00
-            </Text>
-            <Text style={{ color: CT.BG_GRAY_500, fontSize: 12, fontWeight: "600" }}>pm</Text>
-        </React.Fragment>
-    );
+    const Time = ({ date }) => {
+        const d = moment(date);
+        return (
+            <React.Fragment>
+                <Text style={styles.time}>
+                    {d.format(CT.DATE_FORMAT_PRETTY)} <Text style={{ color: CT.BG_GRAY_200 }}>@</Text> {d.format("h:mm")}
+                </Text>
+                <Text style={{ color: CT.BG_GRAY_500, fontSize: 12, fontWeight: "600" }}>{d.format("a")}</Text>
+            </React.Fragment>
+        );
+    };
 
+    const [state, setState] = useState({ index: 0, routes: [] });
     const [loading, setLoading] = useState(true);
-    const [state, setState] = useState({
-        index: 0,
-        routes: [
-            {
-                key: "general",
-                label: "General",
-                data: [
-                    {
-                        text: <Time />,
-                        subtitle: "Petsville Animal Clinic, Cyberjaya",
-                        badge: { text: "Pending" },
-                        tags: [
-                            // { icon: "clock", text: "3:00pm" },
-                            { icon: "stethoscope", text: "Checkup" },
-                            { icon: "cat", text: "Cheshire" },
-                        ],
-                    },
-                    {
-                        text: <Time />,
-                        subtitle: "Petsville Animal Clinic, Cyberjaya",
-                        badge: { text: "Confirmed" },
-                        tags: [
-                            // { icon: "clock", text: "3:00pm" },
-                            { icon: "stethoscope", text: "Checkup" },
-                            { icon: "cat", text: "Cheshire" },
-                        ],
-                    },
-                ],
-            },
-            {
-                key: "boarding",
-                label: "Boarding",
-                data: [
-                    {
-                        text: <Time />,
-                        subtitle: "Catzonia, Petaling Jaya",
-                        badge: { text: "Pending" },
-                        tags: [
-                            // { icon: "clock", text: "3:00pm" },
-                            { icon: "magic", text: "Grooming" },
-                            { icon: "cat", text: "Cheshire" },
-                        ],
-                    },
-                    {
-                        text: <Time />,
-                        subtitle: "Catzonia, Petaling Jaya",
-                        badge: { text: "Pending" },
-                        tags: [
-                            // { icon: "clock", text: "3:00pm" },
-                            { icon: "concierge-bell", text: "Boarding" },
-                            { icon: "cat", text: "Cheshire" },
-                        ],
-                    },
-                ],
-            },
-            {
-                key: "surgery",
-                label: "Surgery",
-                data: [],
-            },
-            {
-                key: "others",
-                label: "Others",
-                data: [
-                    {
-                        text: <Time />,
-                        subtitle: "Catzonia, Petaling Jaya",
-                        badge: { text: "Pending" },
-                        tags: [
-                            // { icon: "clock", text: "3:00pm" },
-                            { icon: "stethoscope", text: "Service" },
-                            { icon: "cat", text: "Cheshire" },
-                        ],
-                    },
-                ],
-            },
-        ],
-    });
+    const [loadingData, setLoadingData] = useState(false);
 
     useEffect(() => {
-        const t = setTimeout(() => {
-            setLoading(false);
-            clearTimeout(t);
-        }, CT.WAITING_DEMO);
+        http.get("/appointments/services")
+            .then(({ data: services }) => {
+                if (services?.length > 0) {
+                    const routes = services.map(({ id, name: label }, i) => {
+                        return { id, key: `${label}_${id}`, label, data: [] };
+                    });
+                    setState({ index: 0, routes });
+                    setLoading(false);
+                }
+            })
+            .catch(({ response }) => net.handleCatch(response, setLoading));
     }, []);
+
+    useEffect(() => {
+        const index = state.index;
+        const routeData = _get(state?.routes, `[${index}]`, {});
+        const serviceID = routeData?.id || null;
+
+        if (serviceID !== null) {
+            http.get(`/appointments/from/service/${serviceID}`)
+                .then(({ data }) => {
+                    if (data?.length > 0) {
+                        let routes = _clone(state?.routes);
+                        let item = data.map(({ date, service, pet, veterinar: vet }) => {
+                            return {
+                                text: <Time date={date} />,
+                                subtitle: [vet?.name, vet?.city].join(", "),
+                                badge: { text: "Pending" },
+                                tags: [
+                                    { icon: "magic", text: service?.name },
+                                    { icon: "cat", text: pet?.name },
+                                ],
+                            };
+                        });
+                        routes[index].data = item;
+                        setState({ index, routes });
+                    }
+                })
+                .catch(({ response }) => net.handleCatch(response, setLoadingData));
+        }
+    }, [state?.index, _get(state, "routes[0].id")]);
 
     const _onIndexChange = (index) => setState({ ...state, index });
     const _onPressItem = (index) => navigation.navigate("appointment__details");
@@ -136,7 +111,13 @@ const AppointmentScreen = ({ navigation }) => {
     const _renderScene = SceneMap(_createSceneMap(state?.routes, _onPressItem, Scene, {}, loading));
     const _renderTabBar = ({ navigationState: state }) => (
         <Header style={styles.header}>
-            <Tabs tabs={state?.routes} active={state?.index} onPress={_onIndexChange} alwaysBounceHorizontal={false} />
+            <Tabs
+                loading={loading}
+                tabs={state?.routes}
+                active={state?.index}
+                onPress={_onIndexChange}
+                alwaysBounceHorizontal={false}
+            />
         </Header>
     );
 
