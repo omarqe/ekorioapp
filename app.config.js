9a86e7645e3aa58fe8d145ad5710d5b624a4a418
Omar Mokhtar <omar.veyron@gmail.com>
2021-10-27 23:29:55 +0800

    :recycle: Update app

--- app.config.js
+++ app.config.js
@@ -4,8 +4,9 @@ export default {
     version: "1.0.0",
     extra: {
         DEVENV: process.env.DEVENV,
-        API_HOST: process.env.API_HOST,
         API_SECRET: process.env.API_SECRET,
+        API_HOST: process.env.API_HOST,
+        API_HOST_DEV: process.env.API_HOST_DEV,
         API_HOST_PORTABLE: process.env.API_HOST_PORTABLE,
     },
 };
