74d91c427ce05c5226fb0f5d729a109dde0ce844
Omar Mokhtar <omar.veyron@gmail.com>
2021-12-21 21:08:51 +0800

    :recycle: Refactor base URL

--- src/functions/http.js
+++ src/functions/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? process.env.API_HOST : process.env.API_HOST_PORTABLE,
+    baseURL: process.env.NODE_ENV === "production" ? process.env.API_HOST : process.env.API_HOST_DEV,
 });
 
 export default http;
