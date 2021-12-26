28a6e120a6f6dac4c913e5b30342ebbe3b3d1e57
Omar Mokhtar <omar.veyron@gmail.com>
2021-12-26 17:32:46 +0800

    :recycle: Update base URL

--- src/functions/getPetImageSrc.js
+++ src/functions/getPetImageSrc.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export default function getPetImageSrc(filename) {
     const [baseURL, setBaseURL] = useState();
     const imageURL = `${baseURL}/assets/pets/${filename}`;
-    useEffect(() => setBaseURL(process.env.API_HOST_DEV), []);
+    useEffect(() => setBaseURL(process.env.NODE_ENV === "production" ? process.env.API_HOST : process.env.API_HOST_DEV), []);
 
     return imageURL;
 }
